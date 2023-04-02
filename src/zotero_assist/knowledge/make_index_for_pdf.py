c8b32e928c8beebb925fb96926273c40e0b90ef2
Bernhard Raml <bernhard.raml@geo.tuwien.ac.at>
2023-04-02 16:09:36 +0200

    F!! use correct model for queries

--- src/zotero_assist/knowledge/make_index_for_pdf.py
+++ src/zotero_assist/knowledge/make_index_for_pdf.py
@@ -10,8 +10,7 @@ from zotero_assist.zot.read_pdf_pages import read_pdf_pages
 SELECTED_MODEL = "text-ada-001"
 
 
-def make_index_for_pdf(pdf_file: Path) -> GPTTreeIndex:
-    llm_predictor = LLMPredictor(llm=OpenAI(model_name=SELECTED_MODEL))
+def make_index_for_pdf(pdf_file: Path, llm_predictor) -> GPTTreeIndex:
     paper_pages = read_pdf_pages(pdf_file)
     index = GPTTreeIndex(make_embedded_docs_from_pages(paper_pages), llm_predictor=llm_predictor)
     index.save_to_disk(get_llaman_index_info_for_pdf(pdf_file)[0] / "index.json")
--- src/zotero_assist/knowledge/retrieve_llama_index_for_pdf.py
+++ src/zotero_assist/knowledge/retrieve_llama_index_for_pdf.py
@@ -1,16 +1,19 @@
 from pathlib import Path
 
 import streamlit as st
+from langchain import OpenAI
+from llama_index import LLMPredictor
 
 from zotero_assist.constants import get_llaman_index_info_for_pdf
 from zotero_assist.knowledge.make_index_for_pdf import make_index_for_pdf
 
 
 def retrieve_llama_index_for_pdf(pdf_file: Path):
+    predictor = LLMPredictor(llm=OpenAI(model_name=st.session_state.index_model))
     index_dir, index_cls = get_llaman_index_info_for_pdf(pdf_file)
     index_file = index_dir / "index.json"
     if index_file.exists():
-        return index_cls.load_from_disk(index_file.as_posix())
+        return index_cls.load_from_disk(index_file.as_posix(), llm_predictor=predictor)
     else:
         with st.spinner("generating index..."):
-            return make_index_for_pdf(pdf_file)
+            return make_index_for_pdf(pdf_file, llm_predictor=predictor)
